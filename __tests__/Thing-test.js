f2537450b754e25fa092bb69e9a899f6206c4880
kevincoleman <kevin.n.coleman@gmail.com>
2019-05-20 15:15:33 -0700

    Add first basic snapshot test

---
+++ __tests__/Thing-test.js
@@ -0,0 +1,35 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import Thing from '../components/thing/Thing';
+import renderer from 'react-test-renderer';
+
+it("renders correctly", () => {
+  renderer.create(<Thing />);
+});
+
+it("renders a default thing if nothing is passed to it", () => {
+  let tree = renderer.create(<Thing />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+// renders a thing that’s passed to it (restructure?)
+  // renders a completed thing if the thing is completed
+  // uses the background color of the thing passed to it
+
+
+// updates the thing when a new thing is gotten
+
+// renders a CompleteButton
+
+// updates notification schedule
+  // if after 9am
+    // schedule for tomorrow at 9am
+  // if before 9am
+    // if nothing completed
+      // schedule for 9am today
+    // if item completed
+      // schedule for tomorrow at 9am
