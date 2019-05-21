b8112a1b273df3006b4966caef1681002f4f3a31
kevincoleman <kevin.n.coleman@gmail.com>
2019-05-20 19:32:27 -0700

    Rearrange new thing retrieval to be offline-first

--- core/Things/Things.js
+++ core/Things/Things.js
@@ -18,10 +18,25 @@ export class Things {
   }
 
   async getNewThing() {
-    // handle new thing
+
+    // get new random thing from local storage
+
+    // get all the things from API
     const things = await thingGateway.all();
-    let thing = things[Math.floor(Math.random() * things.length)];
+    
+    // store all the things into local storage
+    await storage.store("allThings", JSON.stringify(things));
+
+    // retrieve from local storage
+    let localThings = JSON.parse(await storage.retrieve("allThings"));
+
+    // get a random thing
+    let thing = localThings[Math.floor(Math.random() * localThings.length)];
+
+    // init the thing
     thing = this.initThing(thing);
+
+    // put the thing in local storage
     await storage.store("todaysThing", JSON.stringify(thing));
 
     // track event in GA
