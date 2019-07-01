59f8ca1de0639e3ac0886b2220adb14440fa3d0f
kevincoleman <kevin.n.coleman@gmail.com>
2019-07-01 12:53:36 -0700

    Add skipThing tests and reorganize

--- __tests__/ThingsTest.js
+++ __tests__/ThingsTest.js
@@ -4,6 +4,7 @@ import { MockThingGateway } from "../mocks/MockThingGateway.js";
 import { MockStorage } from "../mocks/MockStorage.js";
 import { MockTracker } from "../mocks/MockTracker.js";
 import { MockNotifications } from "../mocks/MockNotifications.js";
+import { isGenericTypeAnnotation } from "@babel/types";
 
 describe("Things", () => {
   let things;
@@ -20,44 +21,70 @@ describe("Things", () => {
     things = new Things(thingGateway, storage, tracker, notifications);
   });
 
-  it("should initialize a given thing", () => {
-    let thing = thingGateway.all()[0]; 
-    expect(thing.title).toEqual("Smile at someone.");
-    expect(thing.completed).toBe(undefined);
-    expect(thing.dateRetrieved).toBe(undefined);
-    expect(thing.color).toBe(undefined);
-    expect(thing.id).toEqual(0);
-
-    thing = things.initThing(thing);
-    expect(thing.title).toEqual("Smile at someone.");
-    expect(thing.completed).toEqual(false);
-    expect(thing.dateRetrieved).toEqual(new Date().toDateString());
-    expect(thing.color).toMatch(/^#+[a-fA-F0-9]+$/);
-    expect(thing.id).toEqual(0);
-  })
-
-  it("should set a new thing in app state", async () => {
-    things.state.todaysThing = thingGateway.all()[0];
-    things.state.todaysThing.title = "oldThing";
-    expect(things.state.todaysThing.title).toEqual("oldThing");
-    
-    await things.getNewThing();
-    expect(things.state.todaysThing.title).not.toEqual("");
-    expect(things.state.todaysThing.title).not.toEqual("oldThing");
+  describe("getThing()", () => {
+    it("should...", () => {
+      // ...
+    })
   });
 
-  it("should complete today’s thing", () => {
-    // setup
-    let thing = thingGateway.all()[0];
-    thing = things.initThing(thing);
-    things.state.todaysThing = thing;
-    expect(things.state.todaysThing.completed).toEqual(false);
+  describe("skipThing()", () => {
+    it("should track the last thing as a 'skipThing' event", () => {
+      things.state.todaysThing = thingGateway.all()[0];
+      spyTracker = jest.spyOn(tracker, "trackEvent");
 
-    things.completeThing();
-    expect(things.state.todaysThing.completed).toEqual(true);
+      things.skipThing();
+      expect(spyTracker).toHaveBeenCalledWith("skipThing", { thing: things.state.todaysThing });
+    })
+
+    it("should try to get a new thing", async () => {
+      spyGetNewThing = jest.spyOn(things, "getThing");
+
+      things.skipThing();
+      expect(spyGetNewThing).toHaveBeenCalledTimes(1);
+    })
+  });
+
+  describe("getNewThing()", () => {
+    it("should set a new thing in app state", async () => {
+      things.state.todaysThing = thingGateway.all()[0];
+      things.state.todaysThing.title = "oldThing";
+      expect(things.state.todaysThing.title).toEqual("oldThing");
+      
+      await things.getNewThing();
+      expect(things.state.todaysThing.title).not.toEqual("");
+      expect(things.state.todaysThing.title).not.toEqual("oldThing");
+    });
   })
-    // complete thing
 
-    // skip thing
+  describe("initThing()", () => {
+    it("should initialize a given thing", () => {
+      let thing = thingGateway.all()[0]; 
+      expect(thing.title).toEqual("Smile at someone.");
+      expect(thing.completed).toBe(undefined);
+      expect(thing.dateRetrieved).toBe(undefined);
+      expect(thing.color).toBe(undefined);
+      expect(thing.id).toEqual(0);
+  
+      thing = things.initThing(thing);
+      expect(thing.title).toEqual("Smile at someone.");
+      expect(thing.completed).toEqual(false);
+      expect(thing.dateRetrieved).toEqual(new Date().toDateString());
+      expect(thing.color).toMatch(/^#+[a-fA-F0-9]+$/);
+      expect(thing.id).toEqual(0);
+    })
+  })
+
+  describe("completeThing()", () => {
+    it("completeThing() should complete today’s thing", () => {
+      // setup
+      let thing = thingGateway.all()[0];
+      thing = things.initThing(thing);
+      things.state.todaysThing = thing;
+      expect(things.state.todaysThing.completed).toEqual(false);
+  
+      things.completeThing();
+      expect(things.state.todaysThing.completed).toEqual(true);
+    })
+  })
 
 });
